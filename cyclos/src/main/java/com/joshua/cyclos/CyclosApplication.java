0438a9d5db4a081a59496e2acec13942facd09bb
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-06 12:42:38 +0200

    cyclos init done ☑️

---
+++ cyclos/src/main/java/com/joshua/cyclos/CyclosApplication.java
@@ -0,0 +1,13 @@
+package com.joshua.cyclos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyclosApplication.class, args);
+	}
+
+}
---
+++ cyclos/src/test/java/com/joshua/cyclos/CyclosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshua.cyclos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
